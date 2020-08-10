17a1e8e1abf0870c123b6469e0713c29acae385b
Casweeney Ojukwu <casweeno2000@gmail.com>
2020-08-11 00:42:05 +0100

    Initial Commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'vMAQiWwISLKLjMdtlUXZgw');
+define('CLIENT_SECRET', '4hxo3Gx7c7q8oaaNU2L3dbDBc0xKchuT');
+define('REDIRECT_URI', 'http://225606acdf3a.ngrok.io/zoom_api/callback.php');
